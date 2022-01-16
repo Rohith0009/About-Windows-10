744976fd53bdd4c20d25ea3e1c199ae450669041
Rohith M <rohithmerala@gmail.com>
2022-01-16 14:41:20 +0530

    V2.0

---
+++ main.js
@@ -0,0 +1,19 @@
+//Extra Stuff Starts
+
+//Smooth Scrolling Starts
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+  //smooth scroling ends
+  
+  //Extra Stuff Ends
