60e2329a35f5edcecb89e15d1e1b9c147ed98c64
Rohith M <rohithmerala@gmail.com>
2022-01-20 13:05:47 +0530

    V2.1

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 
 //Smooth Scrolling Starts
 $(document).ready(function () {
-    $("a").on('click', function (event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
           window.location.hash = hash;
-        });
-      }
-    });
+        }
+      );
+    }
   });
-  //smooth scroling ends
-  
-  //Extra Stuff Ends
+});
+//smooth scroling ends
+
+//Extra Stuff Ends
